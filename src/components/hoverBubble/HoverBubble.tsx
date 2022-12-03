5415f8b94f386f7c1b22434bd24ad36c78b78840
Michael Fernandes <m.fern93@gmail.com>
2022-12-02 16:37:37 -0800

    animations

--- src/components/hoverBubble/HoverBubble.tsx
+++ src/components/hoverBubble/HoverBubble.tsx
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import { SimulationNodeDatum } from "d3";
 import { SyntheticEvent, useEffect, useMemo, useRef } from "react";
 import "./HoverBubble.scss";
 
@@ -15,14 +16,17 @@ interface ForceNode {
 // const tableauColors = d3.scaleOrdinal(d3.range(numNodes), ['transparent'].concat(d3.schemeTableau10));
 const gradient = d3.interpolateCool;
 const PAGE_PADDING_X = 20 * 2;
+let setDpr: boolean;
 
 // Ported from: https://observablehq.com/@d3/collision-detection/2
-export default function HoverBubble({ }) {
+export default function HoverBubble({ showDots }: { showDots: boolean }) {
   const isSmallerScreen = window.innerWidth < 500;
+
   const width = isSmallerScreen ? (window.innerWidth - PAGE_PADDING_X) * 2 : 1000;
+
   const height = width;
   const radius = isSmallerScreen ? 3 : 4;
-  const forceHat = isSmallerScreen ? 0.001 : 0.0023;
+  const forceHat = isSmallerScreen ? 0.001 : 0.00075;
   const numNodes = isSmallerScreen ? 150 : 200;
   const ref = useRef(null);
   console.log(width);
@@ -43,7 +47,20 @@ export default function HoverBubble({ }) {
       // Shout out MBostock
       const canvas = ref.current as HTMLCanvasElement;
       const ctx = canvas.getContext("2d");
+      const rect = canvas.getBoundingClientRect();
 
+      const dpr = window.devicePixelRatio;
+
+      // Set the "actual" size of the canvas
+      canvas.width = rect.width * dpr;
+      canvas.height = rect.height * dpr;
+
+      // Scale the context to ensure correct drawing operations
+      ctx?.scale(dpr, dpr);
+
+      // Set the "drawn" size of the canvas
+      canvas.style.width = `${rect.width}px`;
+      canvas.style.height = `${rect.height}px`;
       const pointed = (event: SyntheticEvent) => {
         let [x, y] = d3.pointer(event);
 
@@ -66,8 +83,8 @@ export default function HoverBubble({ }) {
           for (let i = 0; i < nodes.length; i++) {
             const d = nodes[i];
             ctx.beginPath();
-            ctx.moveTo((d.x || 0) + d.r, d.y || 0);
-            ctx.arc(d.x || 0, d.y || 0, d.r, 0, 2 * Math.PI);
+            ctx.moveTo((d.x || 100) + d.r, d.y || 0);
+            ctx.arc(d.x || 100, d.y || 0, d.r, 0, 2 * Math.PI);
             ctx.fillStyle = d.id ? d.group : "transparent";
             ctx.globalAlpha = 0.9;
             ctx.fill();
@@ -80,11 +97,10 @@ export default function HoverBubble({ }) {
 
       const simulation = d3
         .forceSimulation<ForceNode>(nodes)
-        .alphaTarget(0.2) // stay hot
-        .velocityDecay(0.02) // low friction
+        .alphaTarget(0.1) // stay hot
+        .velocityDecay(0.025) // low friction
         .force("x", d3.forceX().strength(forceHat))
         .force("y", d3.forceY().strength(forceHat))
-        // Todo: fix typing
         .force(
           "collide",
           d3
@@ -107,19 +123,22 @@ export default function HoverBubble({ }) {
       console.log('sizing');
       return () => simulation.stop();
     }
-  }, [ref, window.innerWidth]);
+  }, [ref, window.innerWidth, showDots]);
 
   return (
     <section
       className="hover-bubble-container"
       style={{ width: width / 2, height: height / 2 }}
     >
-      <canvas
-        className="hover-bubble"
-        height={height}
-        width={width}
-        ref={ref}
-      />
+      {
+        showDots &&
+        <canvas
+          className="hover-bubble"
+          height={height}
+          width={width}
+          ref={ref}
+        />
+      }
     </section>
   );
 }
--- src/containers/Landing/Landing.tsx
+++ src/containers/Landing/Landing.tsx
@@ -1,18 +1,38 @@
 import HoverBubble from "../../components/hoverBubble/HoverBubble";
 import ContentWrapper from "../layout/ContentWrapper";
+import { animated, easings, useSpring } from 'react-spring';
+import { useState } from "react";
+
 import "./Landing.scss";
 type Props = {};
 
 export default function Landing({ }: Props) {
+  const [showDots, setDotsVisible] = useState(false);
+
+  const fadeInUp = useSpring({
+    from: { y: 80, opacity: 0 },
+    to: { y: 0, opacity: 1 },
+    delay: 100,
+    config: { duration: 550, easing: easings.easeInCubic }
+  })
+
+  const fadeInLeft = useSpring({
+    from: { x: 80, opacity: 0 },
+    to: { x: 0, opacity: 1 },
+    delay: 500,
+    config: { duration: 550, easing: easings.easeInCubic, },
+    onRest: () => setDotsVisible(true),
+  })
+
   return (
     <ContentWrapper>
       <section className="landing">
-        <div className="landing-desc">
+        <animated.div className="landing-desc">
           {/* <h2 className="subtitle">Cocreation, data heavy and results.</h2> */}
           {/* <h1 className="title">
             Exploring the affect of data upon design from all directions.
           </h1> */}
-          <div className="hero-text">
+          <animated.div className="hero-text" style={fadeInUp} >
             <h1 className="hero-text-p1">
               {/* Envisioning, a more balanced future where the most disadvantaged benefit. */}
               {/*  what is the value */}
@@ -21,11 +41,11 @@ export default function Landing({ }: Props) {
             <h1 className="hero-text-p2">
               with what matters.
             </h1>
-          </div>
-          <h2 className="subtitle">Designer • Developer • Human-Computer Interaction</h2>
-        </div>
-        <HoverBubble />
+          </animated.div>
+          <animated.h2 className="subtitle" style={fadeInLeft}>Designer • Developer • Human-Computer Interaction</animated.h2>
+        </animated.div>
+        <HoverBubble showDots={showDots} />
       </section>
-    </ContentWrapper>
+    </ContentWrapper >
   );
 }
