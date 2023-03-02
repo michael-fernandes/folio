7e20aa6334a1fc3a91de6b310f722a5c74fb8d71
Michael Fernandes <m.fern93@gmail.com>
2023-03-01 22:16:36 -0800

    add covid pics

--- src/projects/covid/Covid.tsx
+++ src/projects/covid/Covid.tsx
@@ -7,63 +7,65 @@ type Props = {}
 
 export default function Covid({ }: Props) {
   return (
-    <>
-      <ContentWrapper>
-        <div className="overview container">
-          <div className="overview-title">
-            <h1>An Ancedotal Design Reflection on COVID-19 Visualization.</h1>
-          </div>
-          <div className="overview-description">
-            <h4 className="overview-name">Role</h4>
-            <span className="overview-item">Development</span><br /><br />
+    <ContentWrapper>
+      <div className="overview container">
+        <div className="overview-title">
+          <h1>An Ancedotal Design Reflection on COVID-19 Visualization.</h1>
+        </div>
+        <div className="overview-description">
+          <h4 className="overview-name">Role</h4>
+          <span className="overview-item">Development</span><br /><br />
 
-            <span className="overview-summary">
-              <i>Covid visualization that started in the first week of march 2020.</i>
-            </span>
-          </div>
+          <span className="overview-summary">
+            <i>Covid visualization that started in the first week of march 2020.</i>
+          </span>
         </div>
-        <div className="content">
+      </div>
+      <div className="content">
+        <p>
+          When the pandemic first started, I was working at IHME creating web tools and other software for disease modeling research. In the early days of the pandemic, one of the first outbreaks happened in Washington state placing IHME at the forefront of a battle to model the outbreak of COVID-19 for months to come. What unfolded next was perhaps the international community's first collective encounter with a predictive model.  Below can access the web tool that I contributed to professionally in the link below. However, after visiting the link, I will revisit the visualizations and highlight what changes might be done if my organization could revisit those early days of the pandemic.
+        </p>
+        <div className="overview-button-container flex center-content">
           <p>
-            When the pandemic first started, I was working at IHME creating web tools and other software for disease modeling research. In the early days of the pandemic, one of the first outbreaks happened in Washington state placing IHME at the forefront of a battle to model the outbreak of COVID-19 for months to come. What unfolded next was perhaps the international community's first collective encounter with a predictive model.  Below can access the web tool that I contributed to professionally in the link below. However, after visiting the link, I will revisit the visualizations and highlight what changes might be done if my organization could revisit those early days of the pandemic.
-          </p>
-          <div className="overview-button-container flex center-content">
-            <p>
-              <ActionLink href="https://covid19.healthdata.org/global" alt="ihme covid visualizations">Visit interactive experience</ActionLink>
-            </p>
-          </div>
-        </div>
-        <img src={WhiteHouse} className="cover-1-img" />
-        <div className="content">
-          To me, perhaps the biggest problem with COVID-19 forecasts might not only have been people's disbelief or rejection of them but also the rejection of other people's disbelief. While COVID-19 models did not exist in a vacuum I often wonder what the public health community I had an inside seat to at the time, could have done better to make our predictions more effective, usable and understandable for the broad range of the public that ended up interfacing with them.
-        </div>
-        <div className="content">
-          The following provides a design reflection on what my team could have done differently to better serve the general population user base that eventually used our decision-making tool. The following interactive covers uncertainty communication improvements and alternative accessible representations of the predictions and suggests a broader design philosophy that could be employed to create these visualizations in the future.
-        </div>
-        <CovidScroller />
-        <div className="content">
-          <p>
-            The stakes around decisions made by the model were high and created a situation where nearly everyone became indirect stakeholders of the technology output. In the United States, especially politically. With so many indirect stakeholders, broadcasting predictive outputs in a way that aligns with the values of all of them sounds hard maybe impossible. Nonetheless more widespread buy-in (and user acceptance) from the larger population could lead to better achievement of the Public Health goals that were initially behind covid modeling.
+            <ActionLink href="https://covid19.healthdata.org/global" alt="ihme covid visualizations">Visit interactive experience</ActionLink>
           </p>
         </div>
-        <div className="content">
-          <p>
-            Moving towards a participatory design strategy of communication seems scary. Giving up control of the output seems like it could lead to undesirable public health messaging. And in the early days of COVID-19 it was totally reasonable to want to control for unknown factors. Using a participatory strategy that included a wide range of views important to the design challenge, could lead to an end product with a wider range of user acceptance and usability.
-          </p>
-        </div>
-        {/* visual image of the screen shot */}
+      </div>
+      <img src={WhiteHouse} className="cover-1-img" />
+      <div className="content">
+        To me, perhaps the biggest problem with COVID-19 forecasts might not only have been people's disbelief or rejection of them but also the rejection of other people's disbelief. While COVID-19 models did not exist in a vacuum I often wonder what the public health community I had an inside seat to at the time, could have done better to make our predictions more effective, usable and understandable for the broad range of the public that ended up interfacing with them.
+      </div>
+      <div className="content">
+        The following provides a design reflection on what my team could have done differently to better serve the general population user base that eventually used our decision-making tool. The following interactive covers uncertainty communication improvements and alternative accessible representations of the predictions and suggests a broader design philosophy that could be employed to create these visualizations in the future.
+      </div>
+      <CovidScroller />
+      <div className="content">
+        <p>
+          The stakes around decisions made by the model were high and created a situation where nearly everyone became indirect stakeholders of the technology output. In the United States, especially politically. With so many indirect stakeholders, broadcasting predictive outputs in a way that aligns with the values of all of them sounds hard maybe impossible. Nonetheless more widespread buy-in (and user acceptance) from the larger population could lead to better achievement of the Public Health goals that were initially behind covid modeling.
+        </p>
+      </div>
+      <div className="content">
+        <p>
+          Moving towards a participatory design strategy of communication seems scary. Giving up control of the output seems like it could lead to undesirable public health messaging. And in the early days of COVID-19 it was reasonable to want to control for unknown factors. Using a participatory strategy that included a wide range of views important to the design challenge, could lead to an end product with a wider range of user acceptance and usability.
+        </p>
+        <p>
+          Small post note: My thoughts here are a work in progress, and need a bit more research to reinforce. It's been hard to get this out, I think partly because those early days of the pandemic were quite stressful, I'm sure as many people can relate.
+        </p>
+      </div>
+      {/* visual image of the screen shot */}
 
-        {/* <p>
+      {/* <p>
             Nearly as soon as the models were published to the web, critiques and questions of their accuracy began. Even with error bands, early modeling was never intended to be a perfect prediction. The few models acted as a stimulus to spur leadership into action.  Take for example two of the more famous early models,  IHME and the imperial college of London model.  The ICL alerted of us of the scale of the problem(millions of lives lost in United States), while IHME told forecasted an exponential growth much early then resources were being deployed (early April 2020 in the United States). Later in the pandemic models still maintained their usefulness at alerting the public alongside decision makers to cases where hospital faced eminent collapse or the trend of infection over time.
           </p> */}
 
-        <br />
-        <br />
-        {/* <p>
+      <br />
+      <br />
+      {/* <p>
             There’s plenty of research on what Public Health might do better next time(hopefully not anytime soon) around for modeling and how to make it more accurate[1,2…]. Beyond accuracy, there seems to be a some what inevitable problem; that within democracies, these models face  politicalization by opposing sides. In a way the globe's first encounter with a large scale predictive model came in the form of COVID forecast models and their digital access points. The stakes around decisions made by the model were high and created a situation where nearly everyone became indirect stakeholders of the technology output. In the United States, especially politically. With so many indirect stakeholders, broadcasting a predictive outputs in a way that aligns with the values of all of them sounds hard maybe impossible. Nonetheless more widespread buy-in (and user acceptance) from the larger population could lead to better achievement of the Public Health goals that were initially behind covid modeling.
           </p> */}
 
-        <br />
-        {/* <h1>Groups of people and what they valued</h1>
+      <br />
+      {/* <h1>Groups of people and what they valued</h1>
         -some of the media like point estimates <br />
         --Easy to broad cast <br />
         --people saw it as too certain <br />
@@ -71,10 +73,10 @@ export default function Covid({ }: Props) {
         --people had problems looking far out <br />
         --link paper that talks about uncertainty bounds in public health viz <br /> */}
 
-        {/* <h1>How process might of helped </h1>
+      {/* <h1>How process might of helped </h1>
         (Shorter, I need to track down a paper that came out of the lancet and outlines how to make a more participatory process(using a global health POV)). */}
 
-        {/* <br /><br />
+      {/* <br /><br />
           Citations
           <br />
           [1] https://www.thelancet.com/journals/lanmic/article/PIIS2666-5247(21)00029-X/fulltext#bib5
@@ -86,8 +88,7 @@ export default function Covid({ }: Props) {
           Lancet article on user research in global health
           <br /><br />
         </div > */}
-      </ContentWrapper>
-    </>
+    </ContentWrapper>
   )
 }
 
--- src/projects/covid/CovidScroller.tsx
+++ src/projects/covid/CovidScroller.tsx
@@ -1,6 +1,8 @@
 import { useRef, useState } from 'react'
 import { Scrollama, Step } from 'react-scrollama';
+import AltUncertainty from './AltUncertainty';
 import Graph from './Graph';
+import Pd from './imgs/pd.png';
 
 export default function CovidScroller() {
   const scrollerRef = useRef<any>();
@@ -18,32 +20,32 @@ export default function CovidScroller() {
           <Scrollama progress onStepEnter={onStepEnter}>
             <Step data={1}>
               <div className="step">
-                As soon as "COVID" models became a thing, critiques and questions of their accuracy came from every which direction. Although these critiques were valid, and often useful, the earliest modeling was never intended to be a perfect predictions. The few early models (London, IHME) primarily acted as a stimulus to spur leadership into action to respond to the scale and timing of COVID upticks.
+                As soon as "COVID" models became a thing, critiques and questions of their accuracy came from every which direction. Although these critiques were valid, and often useful, the earliest modeling was never intended to be a <a href="https://www.thelancet.com/journals/lanmic/article/PIIS2666-5247(21)00029-X/fulltext#bib5" className="text-link">perfect prediction</a>. The few early models (London, IHME) primarily acted as a stimulus to spur leadership into action to respond to the scale and timing of COVID upticks.
               </div>
             </Step>
             <Step data={2}>
               <div className="step">
-                Overtime, models took on larger role in day to day decesion making for Public Health decesion makers along with members of the public themself. Soon attention to the accuracy of model predictions, particularly in America, caused division. The shift of the model's purpose to inform few Public Health Decision makers, to informing the public on risk came with a shift of strategy. Serving both populations introduced problems that could of been solved with different design choices.
+                Over time, models took on larger roles in day-to-day decision-making for Public Health decision makers along with members of the public themself. Soon attention to the accuracy of model predictions, particularly in America, caused division. The shift of the model's purpose to informing Public Health Decision makers, to informing the public on risk came with a shift of strategy. Serving both populations introduced problems that could have been solved with different design choices.
               </div>
             </Step>
             <Step data={3}>
               <div className="step">
-                Accuracy presented the most obvious problem to mass adoption of COVID models. Presentations of forecasting presented a challenge. The constant remodeling of data yielded new results and just like a weather forecast will fluctuate as the prediction comes closer and closer, a good COVID Forecast would respond to changes in disease variants, seasonality and immunity. Showing that predictions were uncertain, and subject to error was key. Showing uncertainty should of been the default and not an add on.
+                Accuracy presented the most obvious problem to the mass adoption of COVID models. Presentations of forecasting presented a challenge. The constant remodeling of data yielded new results and just like a weather forecast will fluctuate as the prediction comes closer and closer, a good COVID Forecast would respond to changes in disease variants, seasonality and immunity. Showing that predictions were uncertain and subject to error was key. Showing uncertainty should have been the default and not an add-on.
               </div>
             </Step>
             <Step data={4}>
               <div className="step">
-                Anecdotaly, IHME received much feedback (a lot unsolicited from the web) about public persons not understanding uncertainty bands. Retrospectively putting emphasis on the changing nature of the visualization could of disconnected these models from being absolute predictions of the future to mere forecasts.
+                Anecdotally, IHME received much feedback (a lot unsolicited from the web) about public persons not understanding uncertainty bands. Retrospectively emphasizing the changing nature of the visualization could have disconnected these models from being absolute predictions of the future to mere forecasts.
               </div>
             </Step>
             <Step data={5}>
               <div className="step">
-                Providing alternatives to traditional uncertainty encodings might have provided a more interpretable communication of COVID. Take for example the dot plot. Providing tools that both a general audience and informed public Health Decision-Makers could use meant consolidations in affordances for both sides. Providing functionality for flexible use might have led to easier understanding for less skilled health decision makers and professional ones.
+                Providing alternatives to traditional uncertainty encodings might have provided a more interpretable communication of COVID. Take for example the dot plot. Providing tools that both a general audience and informed public Health Decision-Makers could use meant consolidations in affordances for both sides. Providing functionality for flexible use might have led to easier understanding for less skilled health decision-makers and professional ones.
               </div>
             </Step>
             <Step data={6}>
               <div className="step">
-                Even then any new representation would come with nuances in the way the representations would need to be presented. Finding these nuances, in the correct way would be hard, and maybe that's why they weren't done on a massive scale.
+                Even then any new representation would come with nuances in the way the representations would need to be presented. Finding these nuances correctly would be hard, and maybe that's why they weren't done on a massive scale.
               </div>
             </Step>
             <Step data={7}>
@@ -53,23 +55,28 @@ export default function CovidScroller() {
             </Step>
             <Step data={8}>
               <div className="step">
-                Exploring the problem space and testing representations and communication before putting out there, is a key to product development and likely something that would have benefited COVID visualizations early on. Deploying practices like co-design to explore the representation problems, interprobability of public health messaging could lead to better user adoption and a higher belief in COVID representations by larger populations
+                Exploring the problem space and testing representations and communication before putting it out there, is key to product development and likely something that would have benefited COVID visualizations early on. Deploying practices like co-design to explore the representation problems, interprobability of public health messaging could lead to better user adoption and a higher belief in COVID representations by larger populations
               </div>
             </Step>
             <Step data={9}>
               <div className="step">
-                Exploring the problem space and testing representaitons and communication before putting out there, is a key to product development and likely somehting that would of benefited COVID visualizations early on.
+                Exploring the problem space and testing representations and communication before putting it out there, is key to product development and likely something that might have benefited COVID visualizations early on.
               </div>
             </Step>
             <Step data={10}>
               <div className="step">
-                Deploying practices like co-design to explore the representation problems, interprobility of public health messaging could lead to better user adoption and a higher belief in covid representaitons by larger populations.
+                Deploying practices like co-design to explore the interoperability of public health messaging could lead to better user adoption and a higher belief in covid representatives by larger populations.
               </div>
             </Step>
           </Scrollama>
         </div>
         <div className="graphic">
-          <Graph color={step === 1 || step === 2} />
+          {
+            step < 5 ?
+              <Graph color={step === 1 || step === 2} /> : step < 8
+                ? <AltUncertainty />
+                : <img className="cover-2-img" src={Pd} />
+          }
         </div>
       </div>
     </div>
